604f9ef2e2f0cf614f206796050d7ba31331bc04
PrimeTime416 <a2kerr@gmail.com>
2020-11-17 01:51:06 +0000

    more practice

--- anything/identifierPlay.go
+++ anything/identifierPlay.go
@@ -2,10 +2,13 @@ package main
 
 import "fmt"
 
+var y = 100 + 2
+var z int = 10
+
 // this is for practice
 func main() {
+	fmt.Println("z = ", z)
 	x := 42
 	fmt.Println("this is x = ", x)
-	y := 100 + x
 	fmt.Println(y)
 }
